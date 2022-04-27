95f77cf66f86f106768723c86e530387ef6f0de8
jpet96 <joshuapet96@outlook.com>
2022-04-27 17:54:03 +0100

    App

---
+++ lib/data.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+
+const CATEGORIES = [
+
+Category(
+    title: 'Italian',
+    id: 'c1',
+    color: Colors.redAccent
+),
+  Category(
+      title: 'Indian',
+      id: 'c1',
+      color: Colors.redAccent
+  ),
+  Category(
+      title: 'Quick & Easy',
+      id: 'c1',
+      color: Colors.redAccent
+  ),
+  Category(
+      title: 'Under £1',
+      id: 'c1',
+      color: Colors.redAccent
+  ),
+  Category(
+      title: 'Breakfast',
+      id: 'c1',
+      color: Colors.redAccent
+  ),
+  Category(
+      title: 'Mexican',
+      id: 'c1',
+      color: Colors.redAccent
+  ),
+  Category(
+      title: 'Jamaican',
+      id: 'c1',
+      color: Colors.redAccent
+  ),
+
+
+  
+
+
+
+
+
+
+
+
+
+];
